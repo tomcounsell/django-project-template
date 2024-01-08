51cce5568b5c28be7bc9ad3711e90fbd42407a3f
Tom Counsell <other@tomcounsell.com>
2024-01-08 15:59:46 +0700

    remove duplicates

--- apps/user/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class CommonConfig(AppConfig):
-    name = 'apps.user'
-    label = 'user'
-    verbose_name = 'Users & Authentication'
-    default_auto_field = 'django.db.models.BigAutoField'
