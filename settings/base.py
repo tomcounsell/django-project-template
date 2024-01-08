934501fbac8f235fc8f4389df9c47d3b7574cdc3
Tom Counsell <other@tomcounsell.com>
2024-01-08 16:11:05 +0700

    update base settings, documentation

--- settings/base.py
+++ settings/base.py
@@ -57,87 +57,121 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # APPLICATIONS
 DJANGO_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.humanize',
-    'django.contrib.sites',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    # "django_components.safer_staticfiles",  # replaces "django.contrib.staticfiles",
+    "django.contrib.humanize",
+    "django.contrib.sites",
 ]
 
 THIRD_PARTY_APPS = [
-    # 'corsheaders',
-    'storages',
-    'django_extensions',
-    # 'request',
-    # 'timezone_field',
-    # 'widget_tweaks',
-    # 'django_user_agents',
+    "storages",
+    "django_extensions",
+    # "request", # a statistics module for django. It stores requests in a database for admins to see.
+    # "django_user_agents",
     'debug_toolbar',
-    'rest_framework',
-    # 'rest_framework.authtoken',
-    'django_filters',
-    # 'simple_history',
+    "widget_tweaks",
+    "rest_framework",
+    "rest_framework_api_key",
+    "django_filters",
+    "django_htmx",
+    "django_components",
 ]
 
 APPS = [
     'apps.common',
-    'apps.user',
+    # 'apps.integration',
     'apps.communication',
-    # 'apps.api',
     # 'apps.public',
-    # 'apps.dashboard',
+    # 'apps.api',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + APPS
 SITE_ID = 1
 
 MIDDLEWARE = [
-    'apps.common.utilities.database.django_middleware.APIHeaderMiddleware',
-    # 'django_user_agents.middleware.UserAgentMiddleware',
-    # 'django.middleware.gzip.GZipMiddleware',
+    "apps.common.utilities.django_middleware.APIHeaderMiddleware",
+    # "django_user_agents.middleware.UserAgentMiddleware",
+    "django.middleware.gzip.GZipMiddleware",
     'debug_toolbar.middleware.DebugToolbarMiddleware',
-    # 'corsheaders.middleware.CorsMiddleware',
-] + [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-] + [
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    # 'request.middleware.RequestMiddleware',
-    # 'simple_history.middleware.HistoryRequestMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # "request_logging.middleware.LoggingMiddleware",
+    # "corsheaders.middleware.CorsMiddleware",
+    "apps.public.middleware.user_state.AttachUserStateMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    # "request.middleware.RequestMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_htmx.middleware.HtmxMiddleware",
 ]
 
-# LOGIN_REDIRECT_URL = '/dashboard/admin'
-LOGIN_URL = '/account/login'
 
-ROOT_URLCONF = 'settings.urls'
+LOGIN_REDIRECT_URL = "/"
+LOGIN_URL = "/account/login"
+
+ROOT_URLCONF = "settings.urls"
 
 # DATABASES -> SEE VENDOR OR LOCAL SETTINGS
 
-
-TEMPLATES = [{
-    'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': [os.path.join(SITE_ROOT, 'templates'), ],
-    'APP_DIRS': True,
-    'OPTIONS': {
-        'context_processors': [
-            'django.template.context_processors.debug',
-            'django.template.context_processors.request',
-            'django.template.context_processors.media',
-            'django.contrib.auth.context_processors.auth',
-            'django.template.context_processors.static',
-            'django.contrib.messages.context_processors.messages',
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            SITE_ROOT / "templates",
+            # BASE_DIR / "apps" / "public" / "templates",
         ],
+        # "APP_DIRS": True,  # removed for django-components
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                # 'django.template.context_processors.media',
+                "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.static",
+                "django.contrib.messages.context_processors.messages",
+            ],
+            "loaders": [
+                (
+                    "django.template.loaders.cached.Loader",
+                    [
+                        "django.template.loaders.filesystem.Loader",
+                        "django.template.loaders.app_directories.Loader",
+                        "django_components.template_loader.Loader",
+                    ],
+                )
+            ],
+            "builtins": [
+                "django_components.templatetags.component_tags",
+            ],
+        },
     },
-}, ]
+]
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Static files (CSS, JavaScript, Images)
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "/static/"
+# Additional locations of static files
+STATICFILES_DIRS = [
+    # BASE_DIR / "static",
+    BASE_DIR / "apps" / "public" / "static",
+    BASE_DIR / "apps" / "public" / "components",
+]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+mimetypes.add_type("text/javascript", ".js", True)
+mimetypes.add_type("text/css", ".css", True)
+
+WSGI_APPLICATION = "settings.wsgi.application"
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
@@ -153,31 +187,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 50,
 }
-
-# SIMPLE_JWT = {
-#     'ACCESS_TOKEN_LIFETIME': timedelta(days=365),
-#     'REFRESH_TOKEN_LIFETIME': timedelta(days=365),
-#     'ROTATE_REFRESH_TOKENS': False,
-#     'BLACKLIST_AFTER_ROTATION': True,
-#
-#     'ALGORITHM': 'HS256',
-#     'SIGNING_KEY': settings.SECRET_KEY,
-#     'VERIFYING_KEY': None,
-#
-#     'AUTH_HEADER_TYPES': ('Bearer',),
-#     'USER_ID_FIELD': 'id',
-#     'USER_ID_CLAIM': 'user_id',
-#
-#     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-#     'TOKEN_TYPE_CLAIM': 'token_type',
-#
-#     'JTI_CLAIM': 'jti',
-#
-#     'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-#     'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
-#     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
-# }
-
 
 # Password validation
 PASSWORD_RESET_TIMEOUT_DAYS = 7
@@ -202,26 +211,19 @@ REQUEST_IGNORE_PATHS = (
 )
 
 # Internationalization
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
 USE_I18N = False
 USE_L10N = False
 USE_TZ = True
 
-AUTH_USER_MODEL = 'user.User'
+AUTH_USER_MODEL = "common.User"
+LOGIN_URL = "/account/login"
 
-# Static files (CSS, JavaScript, Images)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    # ('user', os.path.join(SITE_ROOT, 'apps/user/static')),
-]
+# Default primary key field type
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # General apps settings
-
 if PRODUCTION or STAGE:
     SECURE_SSL_REDIRECT = True
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
