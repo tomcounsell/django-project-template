bb8968b81a181c6c203cf1e2df9d20e366358f49
Paul Tuck <p6ul.tuck@gmail.com>
2024-11-21 17:46:27 +0700

    feat: Make migrations in virtual environment

--- settings/vendor.py
+++ settings/vendor.py
@@ -1,50 +1,51 @@
 import os
 
 from settings import LOCAL, STAGE, DEMO, PRODUCTION
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # AWS
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', "")
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', "")
-AWS_STORAGE_BUCKET_NAME = AWS_S3_BUCKET_NAME = os.environ.get('AWS_S3_BUCKET_NAME', "")
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
+AWS_STORAGE_BUCKET_NAME = AWS_S3_BUCKET_NAME = os.environ.get("AWS_S3_BUCKET_NAME", "")
 AWS_OPTIONS = {
-    'AWS_ACCESS_KEY_ID': AWS_ACCESS_KEY_ID,
-    'AWS_SECRET_ACCESS_KEY': AWS_SECRET_ACCESS_KEY,
-    'AWS_STORAGE_BUCKET_NAME': AWS_S3_BUCKET_NAME,
+    "AWS_ACCESS_KEY_ID": AWS_ACCESS_KEY_ID,
+    "AWS_SECRET_ACCESS_KEY": AWS_SECRET_ACCESS_KEY,
+    "AWS_STORAGE_BUCKET_NAME": AWS_S3_BUCKET_NAME,
 }
-AWS_DEFAULT_ACL = 'public-read'
-AWS_SNS_NAME = os.environ.get('AWS_SNS_NAME', "")
-AWS_STATIC_URL = 'https://' + AWS_S3_BUCKET_NAME + '.s3.amazonaws.com/'
+AWS_DEFAULT_ACL = "public-read"
+AWS_SNS_NAME = os.environ.get("AWS_SNS_NAME", "")
+AWS_STATIC_URL = "https://" + AWS_S3_BUCKET_NAME + ".s3.amazonaws.com/"
 
 
 # STATIC FILES
 if not LOCAL:
     STATIC_URL = AWS_STATIC_URL
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
     STATICFILES_STORAGE = DEFAULT_FILE_STORAGE
 
 
 CACHES = {
     "default": {
-         "BACKEND": "redis_cache.RedisCache",
-         "LOCATION": os.environ.get('REDIS_URL'),
+        "BACKEND": "redis_cache.RedisCache",
+        "LOCATION": os.environ.get("REDIS_URL"),
     }
 }
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
-# HEROKU DATABASE
-import dj_database_url
-DATABASES = {'default': dj_database_url.config(), }
-# Set DATABASE_URL in config if using other host
-# DATABASE_URL = f"postgresql://{username}:{password}@{host}:5432/{dbname}"
+# # HEROKU DATABASE
+# import dj_database_url
+# DATABASES = {'default': dj_database_url.config(), }
+# # Set DATABASE_URL in config if using other host
+# # DATABASE_URL = f"postgresql://{username}:{password}@{host}:5432/{dbname}"
 
 
 DEFAULT_FROM_EMAIL = "info@example.com"  # if you don't already have this in settings
