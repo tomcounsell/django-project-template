2c97c5d7e2ae3093b0d861899ed1f1bc7751af21
Paul Tuck <p6ul.tuck@gmail.com>
2024-11-24 00:43:02 +0700

    chore: Create dump documents for GPT

--- apps/insights/dump_project.py
+++ apps/insights/dump_project.py
@@ -1,6 +1,6 @@
 import os
 
-output_file = "django-project-template-dump.txt"
+output_file = "dump.txt"
 exclude_dir = "./env"
 file_types = (".py", ".js", ".css", ".html", ".yaml", ".json", ".conf", ".txt")
 
