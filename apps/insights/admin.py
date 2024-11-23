d0460838083363b54621db8a693af6c8d337f313
Paul Tuck <p6ul.tuck@gmail.com>
2024-11-23 19:21:10 +0700

    refactor: Modify docker-compose.yml and register admin.py

--- apps/insights/admin.py
+++ apps/insights/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import DataSummary, SummaryComparison
 
-# Register your models here.
+
+@admin.register(DataSummary)
+class DataSummaryAdmin(admin.ModelAdmin):
+    list_display = ("label", "created_at", "updated_at")
+    search_fields = ("label",)
+
+
+@admin.register(SummaryComparison)
+class SummaryComparisonAdmin(admin.ModelAdmin):
+    list_display = ("summary_1", "summary_2", "comparison_type", "created_at")
+    list_filter = ("comparison_type",)
+    search_fields = ("summary_1__label", "summary_2__label")
